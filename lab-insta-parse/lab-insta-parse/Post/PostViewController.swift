9a823a7761ce1ff3015e3b56b75a2ee897e0b83c
William Reed <wreed2020@fau.edu>
2023-09-29 23:42:53 -0400

    Present Image Picker

--- lab-insta-parse/lab-insta-parse/Post/PostViewController.swift
+++ lab-insta-parse/lab-insta-parse/Post/PostViewController.swift
@@ -24,7 +24,27 @@ class PostViewController: UIViewController {
     }
 
     @IBAction func onPickedImageTapped(_ sender: UIBarButtonItem) {
-        // TODO: Pt 1 - Present Image picker
+        // Pt 1 - Present Image picker
+        // Create a configuration object
+        var config = PHPickerConfiguration()
+
+        // Set the filter to only show images as options (i.e. no videos, etc.).
+        config.filter = .images
+
+        // Request the original file format. Fastest method as it avoids transcoding.
+        config.preferredAssetRepresentationMode = .current
+
+        // Only allow 1 image to be selected at a time.
+        config.selectionLimit = 1
+
+        // Instantiate a picker, passing in the configuration.
+        let picker = PHPickerViewController(configuration: config)
+
+        // Set the picker delegate so we can receive whatever image the user picks.
+        picker.delegate = self
+
+        // Present the picker
+        present(picker, animated: true)
 
     }
 
