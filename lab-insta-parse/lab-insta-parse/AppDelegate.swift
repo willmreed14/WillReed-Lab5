0631cae8e49773b32a8445b5fa8a4475167e17e6
William Reed <wreed2020@fau.edu>
2023-09-29 23:21:31 -0400

    Step 0 - Initialize Parse

---
+++ lab-insta-parse/lab-insta-parse/AppDelegate.swift
@@ -0,0 +1,40 @@
+//
+//  AppDelegate.swift
+//  lab-insta-parse
+//
+//  Created by Charlie Hieger on 10/29/22.
+//
+
+import UIKit
+
+import ParseSwift
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+
+        // Pt 1 - Initialize Parse SDK
+        ParseSwift.initialize(applicationId: "ciC6qb2ZYhqhKfPqSBHE2PZBp2vtXQxbjIpSszOr",
+                              clientKey: "2DCZGVDs3ptLOrf743hFLbawDmG12V4MexdYQ0Xz",
+                              serverURL: URL(string: "https://parseapi.back4app.com")!)
+        //https://github.com/parse-community/Parse-Swift/blob/main/ParseSwift.playground/Sources/Common.swift
+
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+}
