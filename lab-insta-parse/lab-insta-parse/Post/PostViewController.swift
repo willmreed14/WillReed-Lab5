a18dcc638f33fa52e78ec6a5696061c791e7e0b4
William Reed <wreed2020@fau.edu>
2023-09-30 00:15:40 -0400

    Step 6- Compose and Save Post

--- lab-insta-parse/lab-insta-parse/Post/PostViewController.swift
+++ lab-insta-parse/lab-insta-parse/Post/PostViewController.swift
@@ -53,7 +53,44 @@ class PostViewController: UIViewController {
         // Dismiss Keyboard
         view.endEditing(true)
 
-        // TODO: Pt 1 - Create and save Post
+        // Pt 1 - Create and save Post
+        // Unwrap optional pickedImage
+        guard let image = pickedImage,
+              // Create and compress image data (jpeg) from UIImage
+              let imageData = image.jpegData(compressionQuality: 0.1) else {
+            return
+        }
+
+        // Create a Parse File by providing a name and passing in the image data
+        let imageFile = ParseFile(name: "image.jpg", data: imageData)
+
+        // Create Post object
+        var post = Post()
+
+        // Set properties
+        post.imageFile = imageFile
+        post.caption = captionTextField.text
+
+        // Set the user as the current user
+        post.user = User.current
+
+        // Save object in background (async)
+        post.save { [weak self] result in
+
+            // Switch to the main thread for any UI updates
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let post):
+                    print("✅ Post Saved! \(post)")
+
+                    // Return to previous view controller
+                    self?.navigationController?.popViewController(animated: true)
+
+                case .failure(let error):
+                    self?.showAlert(description: error.localizedDescription)
+                }
+            }
+        }
 
 
     }
