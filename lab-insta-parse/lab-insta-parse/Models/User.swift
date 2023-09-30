1e6afd5106caf1cd37ed54869f7a7b3a72321f5f
William Reed <wreed2020@fau.edu>
2023-09-29 23:23:56 -0400

    Create a parse user model

--- lab-insta-parse/lab-insta-parse/Models/User.swift
+++ lab-insta-parse/lab-insta-parse/Models/User.swift
@@ -6,9 +6,25 @@
 //
 
 import Foundation
+import ParseSwift
 
-// TODO: Pt 1 - Import Parse Swift
+// Pt 1 - Create Parse User model
+struct User: ParseUser {
+    // These are required by `ParseObject`.
+    var objectId: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+    var ACL: ParseACL?
+    var originalData: Data?
 
+    // These are required by `ParseUser`.
+    var username: String?
+    var email: String?
+    var emailVerified: Bool?
+    var password: String?
+    var authData: [String: [String: String]?]?
 
-// TODO: Pt 1 - Create Parse User model
+    // Your custom properties.
+    // var customKey: String?
+}
 // https://github.com/parse-community/Parse-Swift/blob/3d4bb13acd7496a49b259e541928ad493219d363/ParseSwift.playground/Pages/3%20-%20User%20-%20Sign%20Up.xcplaygroundpage/Contents.swift#L16
